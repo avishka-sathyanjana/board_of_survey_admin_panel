5d2e8f34c6a57a7a40ec763f1d9153c33179f6ab
Avishka Sathyanjana <baavishka@gmail.com>
2024-03-06 09:49:07 +0530

    style: make styles for login page

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,52 +1,99 @@
-import React, { useState } from 'react';
-import { auth } from './firebaseConfig';
-import { signInWithEmailAndPassword } from 'firebase/auth' ;
+import React, { useState } from "react";
+import { auth } from "../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
 
 function LoginForm() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            // Sign in the user with the email and password, if successful display a message to the screen
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Sign in the user with the email and password, if successful display a message to the screen
 
-            await signInWithEmailAndPassword(
-                auth, email, password
-            );
-            setError('Successfully logged in!');
-        }
-        catch (error) {
-            setError(error.message);    
-        }
+      await signInWithEmailAndPassword(auth, email, password);
+      setError("Successfully logged in!");
+    } catch (error) {
+      setError(error.message);
     }
+  };
+
+  const defaultTheme = createTheme();
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            sx={{
+              m: 1,
+              bgcolor: "secondary.main",
+            }}
+          >
+            <LockOutlinedIcon />
+          </Avatar>
+
+          <Typography component="h1" variant="h4">
+            Sign in
+          </Typography>
+
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <TextField
+              // error={error}
+              // helperText="Incorrect entry."
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             
-
-
-    return(
-        <div>
-            <h2>Login</h2>
-            {error && <p style={{color: 'red'}}>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <input
-                    type='email'
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    placeholder='Email'
-                />
-                <input
-                    type="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    placeholder='Password'
-                />
-                <button type='submit'>Login</button>
-
-            </form>
-        </div>
-    );
-
+            />
+            <button type="submit">Login</button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default LoginForm;
